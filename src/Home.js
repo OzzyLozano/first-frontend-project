d28ead4e09cad5fca41bb72adb1f0da1d3c4494e
ozzy_lzn <oziellozanouwu@gmail.com>
2022-03-01 14:04:46 -0600

    added 'what about me?' in home component

--- src/Home.js
+++ src/Home.js
@@ -4,8 +4,17 @@ const Home = () => {
       <h2>Welcome!!!</h2>
       <div className='ms-2'>
         <h4>This is my first Frontend solo project :D</h4>
-        <div>
-          
+        <div className="ms-2 py-2">
+          <p className="lead mb-1">What about me?</p>
+          <p className="mb-1">
+            I'm a student, I'm learning about web development, and currently I am on Frontend, but eventually I'd like to learn backend.
+          </p>
+          <p className="mb-1">
+            So, my intention is for me to become a software and web developer, those two are my favourite part about programming. I really enjoy creating stuff out of scratch and that's why I'm into frontend that much.
+          </p>
+          <p className="mb-1">
+            The reason I'm looking for a job is so that I can start growing, and of course creating various websites. I use and know html5, css3, bootstrap, javascript, and react; learned trough online courses, though I'm willing to learn specific frameworks and tools to work with in case needed, and keep learning the technologies I'll be needing to become a more useful worker or partner. 
+          </p>
         </div>
       </div>
     </div>
