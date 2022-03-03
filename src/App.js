967306b484ffa006b60f17f0cc750d66c8143dd2
ozzy_lzn <oziellozanouwu@gmail.com>
2022-03-02 21:38:03 -0600

    trying to use github pages to publish

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <Navbar />
         <div className="content">
           <Routes>
-            <Route exact path='/my-first-frontend-project/' element={<Home />} />
+            <Route exact path='/project/' element={<Home />} />
           </Routes>
         </div>
         <Footer />
