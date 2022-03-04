c79b158352acfbe8a882ef1900661db77bcb8fd9
ozzy_lzn <oziellozanouwu@gmail.com>
2022-03-03 20:59:32 -0600

    started the contact component

--- src/Contact.js
+++ src/Contact.js
@@ -1,7 +1,10 @@
 const Contact = () => {
   return (
-    <div>
-      
+    <div className="mx-2">
+      <h2 className="text-center">Contact</h2>
+      <div className="contacto">
+        <p>My e-mail: oziellozanouwu@gmail.com</p>
+      </div>
     </div>
   );
 }
--- src/Footer.js
+++ src/Footer.js
@@ -1,8 +1,11 @@
+import { Link } from 'react-router-dom'
+import Contact from './Contact';
+
 const Footer = () => {
   return (
     <footer className="text-center py-4">
       <p className="small-p my-1">This page was made by Oziel Lozano.</p>
-      <a>Contact me.</a>
+      <Link to="/project/contact">Contact me.</Link>
     </footer>
   );
 }
